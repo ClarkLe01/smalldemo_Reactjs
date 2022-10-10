693df0ffe1b96fc81bc043f23bc3e45e824caafd
waflol <clark.le.goldenowl@gmail.com>
2022-10-11 01:10:28 +0700

    custom hook

--- react-advanced-v18/src/App.js
+++ react-advanced-v18/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import ContextAPI from './tutorial/8-useContext/final/1-context-api';
+import Example from './tutorial/9-custom-hooks/final/1-fetch-example';
 function App() {
   return (
     <div className='container'>
       <h2>Advanced Tutorial</h2>
-      <ContextAPI/>
+      <Example/>
     </div>
   )
 }
